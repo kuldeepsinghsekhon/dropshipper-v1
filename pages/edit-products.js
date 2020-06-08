80adbe24d88461aa00fa4ec2bc12a4262193a705
kuldeep singh <mca.kuldeep.sekhon@gmail.com>
2020-06-08 18:42:00 +0530

    committo revert

--- pages/edit-products.js
+++ pages/edit-products.js
@@ -14,7 +14,6 @@ import {
 import store from 'store-js';
 import gql from 'graphql-tag';
 import { Mutation } from 'react-apollo';
-
 const UPDATE_PRICE = gql`
  mutation productVariantUpdate($input: ProductVariantInput!) {
    productVariantUpdate(input: $input) {
