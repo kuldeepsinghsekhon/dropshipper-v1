41c55904964e6cdc1f0905dda7261d91081d2ef6
kuldeep singh <mca.kuldeep.sekhon@gmail.com>
2020-06-09 05:32:30 +0530

    subscription url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { default: graphQLProxy } = require('@shopify/koa-shopify-graphql-proxy');
 const { ApiVersion } = require('@shopify/koa-shopify-graphql-proxy');
 const Router = require('koa-router');
 const { receiveWebhook, registerWebhook } = require('@shopify/koa-shopify-webhooks');
-const getSubscriptionUrl = require('./server/getSubscriptionUrl');
+//const getSubscriptionUrl = require('./server/getSubscriptionUrl');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
