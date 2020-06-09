d4e21f3229625dfc3f16795af938b24abc2146a0
kuldeep singh <mca.kuldeep.sekhon@gmail.com>
2020-06-09 11:51:36 +0530

    index file changed to default

--- index.js
+++ index.js
@@ -1,45 +1,42 @@
 require('isomorphic-fetch');
+const dotenv = require('dotenv');
+dotenv.config();
 const Koa = require('koa');
 const next = require('next');
 const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
-const dotenv = require('dotenv');
-const { default: graphQLProxy } = require('@shopify/koa-shopify-graphql-proxy');
-const { ApiVersion } = require('@shopify/koa-shopify-graphql-proxy');
 const { verifyRequest } = require('@shopify/koa-shopify-auth');
 const session = require('koa-session');
-const axios =require('axios');
+const { default: graphQLProxy } = require('@shopify/koa-shopify-graphql-proxy');
+const { ApiVersion } = require('@shopify/koa-shopify-graphql-proxy');
 const Router = require('koa-router');
-//const rootRouter = require('./routes/index');
-const {receiveWebhook, registerWebhook} = require('@shopify/koa-shopify-webhooks');
-dotenv.config();
+const { receiveWebhook, registerWebhook } = require('@shopify/koa-shopify-webhooks');
+const getSubscriptionUrl = require('./server/getSubscriptionUrl');
 
-var mongoose = require('mongoose');
-const db = require('./config/keys').mongoURI;
-
-
-  const port = parseInt(process.env.PORT, 10) || 3000;
-  const dev = process.env.NODE_ENV !== 'production';
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
-const path = require('path')
-const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY,HOST} = process.env;
+
+const {
+  SHOPIFY_API_SECRET_KEY,
+  SHOPIFY_API_KEY,
+  HOST,
+} = process.env;
 
 app.prepare().then(() => {
   const server = new Koa();
   const router = new Router();
   server.use(session({ sameSite: 'none', secure: true }, server));
   server.keys = [SHOPIFY_API_SECRET_KEY];
-  const webhook = receiveWebhook({ secret: SHOPIFY_API_SECRET_KEY });
+
   server.use(
     createShopifyAuth({
       apiKey: SHOPIFY_API_KEY,
       secret: SHOPIFY_API_SECRET_KEY,
       scopes: ['read_products', 'write_products'],
-      async  afterAuth(ctx) {
-        const {shop, accessToken} = ctx.session;
-        console.log("App SetupExicuted")
-        console.log("New shopOrigion AppToken "+shop +' '+ accessToken)
-        ctx.cookies.set('shopOrigin', shop, {
+      async afterAuth(ctx) {
+        const { shop, accessToken } = ctx.session;
+        ctx.cookies.set("shopOrigin", shop, {
           httpOnly: false,
           secure: true,
           sameSite: 'none'
@@ -56,67 +53,34 @@ app.prepare().then(() => {
           shop,
           apiVersion: ApiVersion.October19
         });
+
         if (registration.success) {
           console.log('Successfully registered webhook!');
         } else {
           console.log('Failed to register webhook', registration.result);
         }
-      //   var shopuser = new Shop({ shop:shop,accessToken:accessToken });      
-      //   Shop.findOneAndUpdate(
-      //     {shop: myshop}, // find a document with that filter
-      //     {accessToken:newaccessToken }, // document to insert when nothing was found
-      //     {upsert: true, new: true}, // options
-      //     function (err, doc) { // callback
-      //         if (err) {
-      //            console.log(err)
-      //         } 
-      //     }
-      // );      
-        
-        // ctx.cookies.set('accessToken', accessToken, {
-        //   httpOnly: false,
-        //   secure: true,
-        //   sameSite: 'none'
-        // });
-        
-
-       
-        ctx.redirect('/');
-      },
-      
-    }),
-
+        //await getSubscriptionUrl(ctx, accessToken, shop);
+      }
+    })
   );
 
-    router.get('*', verifyRequest(), async function(ctx){
-       await handle(ctx.req, ctx.res);
-      ctx.respond = false;
-      ctx.res.statusCode = 200;
-    });
- 
+  const webhook = receiveWebhook({ secret: SHOPIFY_API_SECRET_KEY });
 
   router.post('/webhooks/products/create', webhook, (ctx) => {
     console.log('received webhook: ', ctx.state.webhook);
   });
-  router.get('/other-routes/test', (ctx) => {
-    console.log('It works');
-    ctx.body="oter html page route"
-  });
-  // module.exports = function *(app){
-   // server.use(rootRouter.routes());
 
+  server.use(graphQLProxy({ version: ApiVersion.April19 }));
+
+  router.get('*', verifyRequest(), async (ctx) => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+    ctx.res.statusCode = 200;
+  });
 
   server.use(router.allowedMethods());
   server.use(router.routes());
-  server.use(graphQLProxy({version: ApiVersion.October19}))
 
-  //mongoose
-  // .connect(
-  //   db,
-  //   { useNewUrlParser: true }
-  // )
-  // .then(() => console.log('MongoDB Connected'))
-  // .catch(err => console.log(err));
   server.listen(port, () => {
     console.log(`> Ready on http://localhost:${port}`);
   });
