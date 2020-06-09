dee413638ffa041d422366adb17caf205aaea663
kuldeep singh <mca.kuldeep.sekhon@gmail.com>
2020-06-09 05:58:18 +0530

    removed extra modules

--- pages/_app.js
+++ pages/_app.js
@@ -4,33 +4,12 @@ import { AppProvider } from '@shopify/polaris';
 import { Provider } from '@shopify/app-bridge-react';
 import Cookies from "js-cookie";
 import '@shopify/polaris/styles.css';
-import translations from '@shopify/polaris/locales/en.json';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-
-const client = new ApolloClient({
-  fetchOptions: {
-    credentials: 'include',
-  },
-});
 
 class MyApp extends App {
-
-  state = {
-    toastActive: false,
-    isLoading:false,
-    isDirty:false,
-    searchActive:false,
-    searchValue:'',
-    userMenuActive:false,
-    mobileNavigationActive:false,
-    modalActive:false
-  };
-
   render() {
     const { Component, pageProps } = this.props;
     const config = { apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
-    
+
     return (
       <React.Fragment>
         <Head>
@@ -38,10 +17,8 @@ class MyApp extends App {
           <meta charSet="utf-8" />
         </Head>
         <Provider config={config}>
-          <AppProvider   i18n={translations}>
-            <ApolloProvider client={client}>
-              <Component {...pageProps} />
-            </ApolloProvider>
+          <AppProvider>
+            <Component {...pageProps} />
           </AppProvider>
         </Provider>
       </React.Fragment>
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
 import { EmptyState, Layout, Page } from '@shopify/polaris';
 import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
-import store from 'store-js';
-import ResourceListWithProducts from '../components/ResourceList';
 
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
 class Index extends React.Component {
   state = { open: false };
   render() {
-    const emptyState = !store.get('ids');
     return (
       <Page>
         <TitleBar primaryAction={{
@@ -22,22 +19,18 @@ class Index extends React.Component {
           onSelection={(resources) => this.handleSelection(resources)}
           onCancel={() => this.setState({ open: false })}
         />
-        {emptyState ? (
-          <Layout>
-            <EmptyState
-              heading="Discount your products temporarily"
-              action={{
-                content: 'Select products',
-                onAction: () => this.setState({ open: true }),
-              }}
-              image={img}
-            >
-              <p>Select products to change their price temporarily.</p>
-            </EmptyState>
-          </Layout>
-        ) : (
-            <ResourceListWithProducts />
-          )}
+        <Layout>
+          <EmptyState
+            heading="Discount your products temporarily"
+            action={{
+              content: 'Select products',
+              onAction: () => this.setState({ open: true }),
+            }}
+            image={img}
+          >
+            <p>Select products to change their price temporarily.</p>
+          </EmptyState>
+        </Layout>
       </Page>
     );
   }
@@ -45,7 +38,7 @@ class Index extends React.Component {
   handleSelection = (resources) => {
     const idsFromResources = resources.selection.map((product) => product.id);
     this.setState({ open: false });
-    store.set('ids', idsFromResources);
+    console.log(resources);
   };
 }
 
